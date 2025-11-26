470d90ee3339075f2de095ce9226b4973a66f24c
JayeshSethi <sethijayesh@gmail.com>
2025-11-25 19:16:11 -0800

    Incoming Updates Added to the side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,21 +6,19 @@ import { JobFormComponent } from './job-form/job-form';
 import { CoverLetterComponent } from './cover-letter/cover-letter'; 
 import { BoardComponent } from './board/board';
 import { ResourcesComponent } from './resources/resources';
+import { UpdatesComponent } from './updates/updates';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'board', component: BoardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'add-job', component: JobFormComponent },
-  
-  // --- NEW: Edit Route (Reuses the same component) ---
   { path: 'edit-job/:id', component: JobFormComponent },
-
   { path: 'cover-letter', component: CoverLetterComponent },
   { path: 'resources', component: ResourcesComponent },
+  { path: 'updates', component: UpdatesComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
-  
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: '/dashboard' } 
 ];
