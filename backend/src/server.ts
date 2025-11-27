acf91846c8093dc4e5942373bd50ca317ebe8394
Ahad Baig <ahadalibaig689@gmail.com>
2025-11-27 00:37:09 -0800

    connected backend to database and created basic tables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-
+import pool from './config/database';
 const app = express();
 const PORT = 3000;
 
 
 app.use(express.json());
+
+// Database
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error executing query', err);
+    } else {
+        console.log('Database connected at', res.rows[0].now);
+    }
+});
 
 // Routes
 app.get('/', (req, res) => {
